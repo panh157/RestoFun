b8a5162af0e47d2287c5fd0c2cf372ca237d4dd6
KONE PANH <PROJET@MacBook-Pro-de-MacBook-4.local>
2018-09-12 18:07:10 +0000

    commit initial

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RestaurantRepository")
+ */
+class Restaurant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=191)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="string", length=191)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=191)
+     */
+    private $contact;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+}
